6c40a88ec676782c6ebcffda9eab3b71d8c6d7c5
huangxiaolu <huangxiaolu@qianxin.com>
2022-09-26 16:15:40 +0800

    chore: add more test case

--- test/test.js
+++ test/test.js
@@ -15,12 +15,70 @@ function sampleProvinceData() {
 }
 
 describe("province", function () {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
+  // 模拟用户改变产量的输入值
+  it('change production', function() {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).equal(-6);
+    expect(asia.profit).equal(292);
+  });
+
+  it('zero demand', function() {
+    asia.demand = 0;
+    expect(asia.shortfall).equal(-25);
+    expect(asia.profit).equal(0);
+  });
+
+  it('negative demand', function() {
+    asia.demand = -1;
+    expect(asia.shortfall).equal(-26);
+    expect(asia.profit).equal(-10);
+  });
+
+  it('empty string demand', function() {
+    asia.demand = "";
+    expect(asia.shortfall).NaN;
+    expect(asia.profit).NaN;
+   });
 });
+
+describe('no producers', function() {
+  let noProducers;
+  beforeEach(function() {
+   const data = {
+    name: "No proudcers",
+    producers: [],
+    demand: 30,
+    price: 20
+   };
+   noProducers = new Province(data);
+  });
+  it('shortfall', function() {
+   expect(noProducers.shortfall).equal(30);
+  });
+  it('profit', function() {
+   expect(noProducers.profit).equal(0);
+  });
+});
+
+// describe('string for producers', function() {
+//   it('', function() {
+//    const data = {
+//     name: "String producers",
+//     producers: "",
+//     demand: 30,
+//     price: 20
+//    };
+//    const prov = new Province(data);
+//    expect(prov.shortfall).equal(0);
+//   });
+// });

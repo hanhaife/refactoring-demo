878bfc68623b24e144b1d64e05668416713ac5f4
lichun01 <lichun01@qianxin.com>
2022-10-10 16:30:46 +0800

    chore: doc

--- src/inline-function/example-2.js
+++ src/inline-function/example-2.js
@@ -1,19 +1,6 @@
 // 内联函数
 // 重构前
 
-// function customerInfo(customer) {
-//   const name = customer.name;
-//   前
-//   const sexual = getSexual(customer);
-//   后
-//   const sexual = customer.sexual === 1 ? "男" : "女";
-//   console.log(name, sexual)
-// }
-
-// function getSexual(customer) {
-//   return customer.sexual === 1 ? "男" : "女";
-// }
-
 function reportLines(aCustomer) {
   const lines = [];
   gatherCustomerData(lines, aCustomer);

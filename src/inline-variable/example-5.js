7397dc48891755573219f1d5ef521f980c16ddd6
lichun01 <lichun01@qianxin.com>
2022-10-10 16:17:26 +0800

    chore: 6章文档

--- src/inline-variable/example-5.js
+++ src/inline-variable/example-5.js
@@ -1,7 +1,7 @@
 // 内联变量
 // 重构前
 
-// let basePrice = anOrder.basePrice;
-// return basePrice > 1000;
+let basePrice = anOrder.basePrice;
+return basePrice > 1000;
 
 // return anOrder.basePrice > 1000;
